8788751313969471c77bc4e884a7e91b07868a27
xiechao <xiechao06>
2019-01-28 17:19:49 +0800

    add more tst

--- test.js
+++ test.js
@@ -24,3 +24,13 @@ test('origin', t => {
   t.equal(o, opp(o)[origin])
   t.end()
 })
+
+test('Object.keys/values/entries', t => {
+  let o = opp({ a: 1, b: 2 })
+  t.ok(Object.keys(o).includes('a'))
+  t.ok(Object.keys(o).includes('b'))
+  t.ok(Object.values(o).includes(1))
+  t.ok(Object.values(o).includes(2))
+  t.deepEqual(Object.entries(o), [['a', 1], ['b', 2]])
+  t.end()
+})
