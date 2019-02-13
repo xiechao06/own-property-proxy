d4b7eca7249f91abee5448146d3e3e6de4f27d46
xiechao <xiechao06>
2019-02-13 09:34:12 +0800

    uglify -> terser

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import babel from 'rollup-plugin-babel'
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import builtins from 'rollup-plugin-node-builtins'
-import { uglify } from 'rollup-plugin-uglify'
+import { terser } from 'rollup-plugin-terser'
 
 export default [
   {
@@ -58,7 +58,7 @@ export default [
       builtins(),
       resolve(),
       commonjs(),
-      uglify()
+      terser()
     ]
   }
 ]
